06a7a14a81a26054f1957c6fd6f4cc98bd8a3084
Sandun Rajitha <rajitha1911@gmail.com>
2022-02-12 14:04:06 +0530

    initial project setup

---
+++ MovieStreamingAppClone/MovieStreamingAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieStreamingAppCloneApp.swift
+//  MovieStreamingAppClone
+//
+//  Created by Sandun Liyanage on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieStreamingAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
